bd62702f90ab3b752a966f1db3891344eca75d97
wrongwrong <boranti1995@gmail.com>
2022-05-07 17:42:48 +0900

    simplify cond

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/InternalCommons.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/InternalCommons.kt
@@ -22,10 +22,6 @@ internal fun Int.toBitSet(): BitSet {
     return bits
 }
 
-// In the future, value classes without @JvmInline will be available, and unboxing may not be able to handle it.
-// https://github.com/FasterXML/jackson-module-kotlin/issues/464
-// The JvmInline annotation can be added to Java classes,
-// so the isKotlinClass decision is necessary (the order is preferable in terms of possible frequency).
-internal fun Class<*>.isUnboxableValueClass() = annotations.any { it is JvmInline } && this.isKotlinClass()
+internal fun Class<*>.isUnboxableValueClass() = annotations.any { it is JvmInline }
 
 internal fun KType.erasedType(): Class<out Any> = this.jvmErasure.java

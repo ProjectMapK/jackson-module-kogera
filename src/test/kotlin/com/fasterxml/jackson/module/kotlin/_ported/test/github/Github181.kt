17fcfe6a9cacc665ec1f10fbeeb5f4c5a8df7827
wrongwrong <boranti1995@gmail.com>
2022-08-07 00:46:02 +0900

    fix annotated constructor formats

--- src/test/kotlin/com/fasterxml/jackson/module/kotlin/_ported/test/github/Github181.kt
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/_ported/test/github/Github181.kt
@@ -12,7 +12,9 @@ class TestGithub181 {
         OK
     }
 
-    data class HealthStatusMap @JsonCreator(mode = JsonCreator.Mode.DELEGATING) constructor(val statuses: Map<String, HealthStatus>) :
+    data class HealthStatusMap
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    constructor(val statuses: Map<String, HealthStatus>) :
         Map<String, HealthStatus> by statuses {
 
         fun isPassing() = statuses.all { (_, v) -> v == HealthStatus.OK }

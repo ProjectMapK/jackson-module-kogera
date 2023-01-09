2f0cf90e48121029acd43b56f6f821c8a8f333ee
wrongwrong <boranti1995@gmail.com>
2023-01-09 22:02:05 +0900

    Modify implementation

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/deser/value_instantiator/creator/ConstructorValueCreator.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/deser/value_instantiator/creator/ConstructorValueCreator.kt
@@ -1,13 +1,60 @@
 package com.fasterxml.jackson.module.kotlin.deser.value_instantiator.creator
 
-import kotlin.reflect.KFunction
-import kotlin.reflect.jvm.isAccessible
+import com.fasterxml.jackson.module.kotlin.defaultConstructorMarker
+import com.fasterxml.jackson.module.kotlin.deser.value_instantiator.argument_bucket.ArgumentBucket
+import com.fasterxml.jackson.module.kotlin.deser.value_instantiator.argument_bucket.BucketGenerator
+import com.fasterxml.jackson.module.kotlin.hasVarargParam
+import com.fasterxml.jackson.module.kotlin.toKmClass
+import com.fasterxml.jackson.module.kotlin.toSignature
+import kotlinx.metadata.KmClass
+import kotlinx.metadata.jvm.signature
+import java.lang.reflect.Constructor
 
-internal class ConstructorValueCreator<T>(override val callable: KFunction<T>) : ValueCreator<T>() {
-    override val accessible: Boolean = callable.isAccessible
+internal class ConstructorValueCreator<T : Any>(private val constructor: Constructor<T>) : ValueCreator<T>() {
+    private val declaringClass: Class<T> = constructor.declaringClass
+
+    override val isAccessible: Boolean = constructor.isAccessible
+    override val callableName: String = constructor.name
+    override val valueParameters: List<ValueParameter>
+    override val bucketGenerator: BucketGenerator
 
     init {
         // To prevent the call from failing, save the initial value and then rewrite the flag.
-        if (!accessible) callable.isAccessible = true
+        if (!isAccessible) constructor.isAccessible = true
+
+        val declaringKmClass: KmClass = declaringClass.toKmClass()
+
+        val constructorParameters = constructor.toSignature().desc.let { desc ->
+            // Compare only desc for performance
+            declaringKmClass.constructors.first { desc == it.signature?.desc }
+        }.valueParameters
+
+        valueParameters = constructorParameters.map { ValueParameter(it) }
+        bucketGenerator = BucketGenerator(constructor.parameterTypes.asList(), constructorParameters.hasVarargParam())
+    }
+
+    private val defaultConstructor: Constructor<T> by lazy {
+        val maskSize = (constructor.parameters.size + Integer.SIZE - 1) / Integer.SIZE
+
+        val defaultTypes = constructor.parameterTypes.let {
+            val parameterSize = it.size
+            val temp = it.copyOf(parameterSize + maskSize + 1)
+            for (i in 0 until maskSize) {
+                temp[it.size + i] = Int::class.javaPrimitiveType
+            }
+            temp[parameterSize + maskSize] = defaultConstructorMarker
+
+            temp
+        }
+
+        declaringClass.getDeclaredConstructor(*defaultTypes).apply {
+            if (!this.isAccessible) this.isAccessible = true
+        }
+    }
+
+    override fun callBy(args: ArgumentBucket): T = if (args.isFullInitialized) {
+        constructor.newInstance(*args.getArgs())
+    } else {
+        defaultConstructor.newInstance(*args.getDefaultArgs())
     }
 }
--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/deser/value_instantiator/creator/MethodValueCreator.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/deser/value_instantiator/creator/MethodValueCreator.kt
@@ -1,52 +1,85 @@
 package com.fasterxml.jackson.module.kotlin.deser.value_instantiator.creator
 
-import com.fasterxml.jackson.module.kotlin.erasedType
-import kotlin.reflect.KFunction
-import kotlin.reflect.KParameter
-import kotlin.reflect.full.extensionReceiverParameter
-import kotlin.reflect.full.instanceParameter
-import kotlin.reflect.jvm.isAccessible
+import com.fasterxml.jackson.module.kotlin.deser.value_instantiator.argument_bucket.ArgumentBucket
+import com.fasterxml.jackson.module.kotlin.deser.value_instantiator.argument_bucket.BucketGenerator
+import com.fasterxml.jackson.module.kotlin.hasVarargParam
+import com.fasterxml.jackson.module.kotlin.toKmClass
+import com.fasterxml.jackson.module.kotlin.toSignature
+import kotlinx.metadata.KmClass
+import kotlinx.metadata.KmFunction
+import kotlinx.metadata.jvm.signature
+import java.lang.reflect.Field
+import java.lang.reflect.Method
 
-internal class MethodValueCreator<T> private constructor(
-    override val callable: KFunction<T>,
-    override val accessible: Boolean,
-    val companionObjectInstance: Any
-) : ValueCreator<T>() {
-    val instanceParameter: KParameter = callable.instanceParameter!!
+internal class MethodValueCreator<T>(private val method: Method) : ValueCreator<T>() {
+    // companion object is always present in the case of a factory function
+    private val companionField: Field
+    private val companionObjectClass: Class<*>
 
-    companion object {
-        fun <T> of(callable: KFunction<T>): MethodValueCreator<T>? {
-            // we shouldn't have an instance or receiver parameter and if we do, just go with default Java-ish behavior
-            if (callable.extensionReceiverParameter != null) return null
+    override val isAccessible: Boolean
+    override val callableName: String = method.name
+    override val valueParameters: List<ValueParameter>
+    override val bucketGenerator: BucketGenerator
 
-            val possibleCompanion = callable.instanceParameter!!.type.erasedType().kotlin
+    init {
+        val declaringClass = method.declaringClass
+        companionField = declaringClass.getDeclaredField(declaringClass.toKmClass().companionObject!!)
 
-            // abort, we have some unknown case here
-            if (!possibleCompanion.isCompanion) return null
+        // region: about accessibility
+        isAccessible = method.isAccessible && companionField.isAccessible
 
-            // To prevent the call from failing, save the initial value and then rewrite the flag.
-            val initialCallableAccessible = callable.isAccessible
-            if (!initialCallableAccessible) callable.isAccessible = true
+        // To prevent the call from failing, save the initial value and then rewrite the flag.
+        if (!method.isAccessible) method.isAccessible = true
+        if (!companionField.isAccessible) companionField.isAccessible = true
+        // endregion
 
-            val (companionObjectInstance: Any, accessible: Boolean) = try {
-                // throws ex
-                val instance = possibleCompanion.objectInstance!!
+        // region: read kotlin metadata information
+        companionObjectClass = companionField.type
+        val companionKmClass: KmClass = companionObjectClass.toKmClass()
+        val kmFunction: KmFunction = run {
+            val signature = method.toSignature()
+            companionKmClass.functions.first { signature == it.signature }
+        }
 
-                // If an instance of the companion object can be obtained, accessibility depends on the KFunction
-                instance to initialCallableAccessible
-            } catch (ex: IllegalAccessException) {
-                // fallback for when an odd access exception happens through Kotlin reflection
-                possibleCompanion.java.enclosingClass.declaredFields
-                    .firstOrNull { it.type.kotlin.isCompanion }
-                    ?.let {
-                        it.isAccessible = true
+        valueParameters = kmFunction.valueParameters.map { ValueParameter(it) }
+        bucketGenerator = BucketGenerator(method.parameterTypes.asList(), kmFunction.valueParameters.hasVarargParam())
+        // endregion
+    }
 
-                        // If the instance of the companion object cannot be obtained, accessibility will always be false
-                        it.get(null) to false
-                    } ?: throw ex
+    private val defaultCaller: (args: Array<Any?>) -> T by lazy {
+        val maskSize = (method.parameters.size + Integer.SIZE - 1) / Integer.SIZE
+        val defaultTypes = method.parameterTypes.let { parameterTypes ->
+            val parameterSize = parameterTypes.size
+
+            // companion object instance(1) + parameterSize + maskSize + marker(1)
+            val temp = arrayOfNulls<Class<*>>(1 + parameterSize + maskSize + 1)
+            temp[0] = companionObjectClass // companion object
+            parameterTypes.copyInto(temp, 1) // parameter types
+            for (i in (parameterSize + 1)..(parameterSize + maskSize)) { // masks
+                temp[i] = Int::class.javaPrimitiveType
             }
+            temp[parameterSize + maskSize + 1] = Object::class.java // maker
+            temp
+        }
+        val defaultMethod = companionObjectClass.getDeclaredMethod("${callableName}\$default", *defaultTypes)
 
-            return MethodValueCreator(callable, accessible, companionObjectInstance)
+        val companionObject = companionField.get(null)
+
+        return@lazy {
+            val actualArgs = arrayOfNulls<Any?>(it.size + 1)
+            actualArgs[0] = companionObject
+            it.copyInto(actualArgs, 1)
+
+            @Suppress("UNCHECKED_CAST")
+            defaultMethod.invoke(null, *actualArgs) as T
         }
     }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun callBy(args: ArgumentBucket): T = if (args.isFullInitialized) {
+        // It calls static method for simplicity, and is a little slower in terms of speed.
+        method.invoke(null, *args.getArgs())
+    } else {
+        defaultCaller(args.getDefaultArgs())
+    } as T
 }

211b1cc107a2ed781eecec7732505a65e8bc51f8
wrongwrong <boranti1995@gmail.com>
2023-01-12 23:23:39 +0900

    Formatting

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/deser/value_instantiator/KotlinValueInstantiator.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/deser/value_instantiator/KotlinValueInstantiator.kt
@@ -141,8 +141,11 @@ internal class KotlinInstantiators(
                     strictNullChecks
                 )
             } else {
-                // TODO: return defaultInstantiator and let default method parameters and nullability go unused?  or die with exception:
-                throw IllegalStateException("KotlinValueInstantiator requires that the default ValueInstantiator is StdValueInstantiator")
+                // TODO: return defaultInstantiator and let default method parameters and nullability go unused?
+                //       or die with exception:
+                throw IllegalStateException(
+                    "KotlinValueInstantiator requires that the default ValueInstantiator is StdValueInstantiator"
+                )
             }
         } else {
             defaultInstantiator

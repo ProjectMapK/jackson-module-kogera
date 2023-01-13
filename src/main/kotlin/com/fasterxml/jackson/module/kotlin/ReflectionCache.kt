dea6232fa31d5d70665e023d03220311ce06fdf5
wrongwrong <boranti1995@gmail.com>
2023-01-14 03:04:13 +0900

    Refactor and formatting

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/ReflectionCache.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/ReflectionCache.kt
@@ -11,15 +11,11 @@ import java.lang.reflect.Constructor
 import java.lang.reflect.Method
 
 internal class ReflectionCache(reflectionCacheSize: Int) {
-    private val javaConstructorToValueCreator = LRUMap<Constructor<*>, ConstructorValueCreator<*>>(reflectionCacheSize, reflectionCacheSize)
-    private val javaMethodToValueCreator = LRUMap<Method, MethodValueCreator<*>>(reflectionCacheSize, reflectionCacheSize)
+    private val javaConstructorToValueCreator =
+        LRUMap<Constructor<*>, ConstructorValueCreator<*>>(reflectionCacheSize, reflectionCacheSize)
+    private val javaMethodToValueCreator =
+        LRUMap<Method, MethodValueCreator<*>>(reflectionCacheSize, reflectionCacheSize)
 
-    /**
-     * return null if...
-     * - can't get kotlinFunction
-     * - contains extensionReceiverParameter
-     * - instance parameter is not companion object or can't get
-     */
     fun valueCreatorFromJava(withArgsCreator: AnnotatedWithParams): ValueCreator<*> = when (withArgsCreator) {
         is AnnotatedConstructor -> {
             val constructor = withArgsCreator.annotated
@@ -39,6 +35,9 @@ internal class ReflectionCache(reflectionCacheSize: Int) {
                     javaMethodToValueCreator.putIfAbsent(method, value) ?: value
                 }
         }
-        else -> throw IllegalStateException("Expected a constructor or method to create a Kotlin object, instead found ${withArgsCreator.annotated.javaClass.name}")
+        else -> throw IllegalStateException(
+            "Expected a constructor or method to create a Kotlin object," +
+                " instead found ${withArgsCreator.annotated.javaClass.name}"
+        )
     } // we cannot reflect this method so do the default Java-ish behavior
 }

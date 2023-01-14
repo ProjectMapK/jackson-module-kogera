177b629c48e1ffa4cb270d853db0b5738dcd74b6
wrongwrong <boranti1995@gmail.com>
2023-01-14 14:35:03 +0900

    Fix error at Jackson2ObjectMapperBuilder

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinFeature.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinFeature.kt
@@ -5,7 +5,7 @@ import java.util.BitSet
 /**
  * @see KotlinModule.Builder
  */
-public enum class KotlinFeature(private val enabledByDefault: Boolean) {
+public enum class KotlinFeature(internal val enabledByDefault: Boolean) {
     /**
      * This feature represents whether to deserialize `null` values for collection properties as empty collections.
      */

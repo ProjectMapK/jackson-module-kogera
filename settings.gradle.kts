6c4909cc5d81c21d6a8067c2e352da77993c4f9a
wrongwrong <boranti1995@gmail.com>
2023-02-26 12:44:17 +0900

    Add kotlin grid test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 rootProject.name = "jackson-module-kogera"
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        System.getenv("KOTLIN_VERSION")?.let { kotlinVersion ->
+            println("Using kotlin version: $kotlinVersion")
+            for (catalog in this) {
+                catalog.version("kotlin", kotlinVersion)
+            }
+        }
+    }
+}

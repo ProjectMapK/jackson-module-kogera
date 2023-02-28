2df25b7dbe7363fb9717f739c6a5159b563e4f1b
wrongwrong <boranti1995@gmail.com>
2023-02-28 18:43:12 +0900

    Downgrade Kotlin version 1
    ${Path to kotlin-stdlib-common}/kotlin-stdlib-common-1.8.0.jar!/META-INF/kotlin-stdlib-common.kotlin_module: Module was compiled with an incompatible version of Kotlin. The binary version of its metadata is 1.8.0, expected version is 1.5.1.
    https://youtrack.jetbrains.com/issue/KT-56970

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     `maven-publish` // for JitPack
 
-    val kotlinVersion: String = System.getenv("KOTLIN_VERSION")?.takeIf { it.isNotEmpty() } ?: "1.7.21"
+    val kotlinVersion: String = System.getenv("KOTLIN_VERSION")?.takeIf { it.isNotEmpty() } ?: "1.5.32"
     kotlin("jvm") version kotlinVersion
 
     java
-    id("org.jmailen.kotlinter") version "3.13.0"
 }
 
 val jacksonVersion = "2.14.2"
@@ -39,14 +38,6 @@ kotlin {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-kotlinter {
-    // see https://github.com/pinterest/ktlint/blob/master/docs/rules/standard.md
-    this.disabledRules = arrayOf(
-        "package-name", // This project allows for the inclusion of _ to represent the package name in the snake case.
-        "filename" // For clarity in future extensions, this rule is disabled.
-    )
 }
 
 tasks {

79b0cd8d80f9945a4e9aabc012eac625ae2062b0
wrongwrong <boranti1995@gmail.com>
2023-04-23 12:09:19 +0900

    Add JsonKey support on value class

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/ser/serializers/KotlinKeySerializers.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/ser/serializers/KotlinKeySerializers.kt
@@ -1,5 +1,6 @@
 package com.fasterxml.jackson.module.kotlin.ser.serializers
 
+import com.fasterxml.jackson.annotation.JsonKey
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.BeanDescription
 import com.fasterxml.jackson.databind.JavaType
@@ -9,6 +10,8 @@ import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.ser.Serializers
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import com.fasterxml.jackson.module.kotlin.isUnboxableValueClass
+import java.lang.reflect.Method
+import java.lang.reflect.Modifier
 
 internal object ValueClassUnboxKeySerializer : StdSerializer<Any>(Any::class.java) {
     override fun serialize(value: Any, gen: JsonGenerator, provider: SerializerProvider) {
@@ -25,13 +28,48 @@ internal object ValueClassUnboxKeySerializer : StdSerializer<Any>(Any::class.jav
     }
 }
 
+// Class must be UnboxableValueClass.
+private fun Class<*>.getStaticJsonKeyGetter(): Method? = this.declaredMethods.find { method ->
+    Modifier.isStatic(method.modifiers) && method.annotations.any { it is JsonKey && it.value }
+}
+
+internal class ValueClassStaticJsonKeySerializer<T>(
+    t: Class<T>,
+    private val staticJsonKeyGetter: Method
+) : StdSerializer<T>(t) {
+    private val keyType: Class<*> = staticJsonKeyGetter.returnType
+    private val unboxMethod: Method = t.getMethod("unbox-impl")
+
+    override fun serialize(value: T, gen: JsonGenerator, provider: SerializerProvider) {
+        val unboxed = unboxMethod.invoke(value)
+        // As shown in the processing of the factory function, jsonValueGetter is always a static method.
+        val jsonKey: Any? = staticJsonKeyGetter.invoke(null, unboxed)
+
+        val serializer = jsonKey
+            ?.let { provider.findKeySerializer(keyType, null) }
+            ?: provider.findNullKeySerializer(provider.constructType(keyType), null)
+
+        serializer.serialize(jsonKey, gen, provider)
+    }
+
+    companion object {
+        // `t` must be UnboxableValueClass.
+        // If create a function with a JsonValue in the value class,
+        // it will be compiled as a static method (= cannot be processed properly by Jackson),
+        // so use a ValueClassSerializer.StaticJsonValue to handle this.
+        fun createOrNull(t: Class<*>): StdSerializer<*>? =
+            t.getStaticJsonKeyGetter()?.let { ValueClassStaticJsonKeySerializer(t, it) }
+    }
+}
+
 internal class KotlinKeySerializers : Serializers.Base() {
     override fun findSerializer(
         config: SerializationConfig,
         type: JavaType,
         beanDesc: BeanDescription
     ): JsonSerializer<*>? = when {
-        type.rawClass.isUnboxableValueClass() -> ValueClassUnboxKeySerializer
+        type.rawClass.isUnboxableValueClass() -> ValueClassStaticJsonKeySerializer.createOrNull(type.rawClass)
+            ?: ValueClassUnboxKeySerializer
         else -> null
     }
 }

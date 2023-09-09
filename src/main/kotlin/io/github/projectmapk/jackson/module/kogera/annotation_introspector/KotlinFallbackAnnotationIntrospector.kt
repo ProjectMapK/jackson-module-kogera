10b56c7bc65cea6a0047d4fb1a89dd7d4beca71a
wrongwrong <boranti1995@gmail.com>
2023-09-10 03:15:59 +0900

    Fixed ClosedRange conversions to be handled by AbstractTypeResolver
    Because it is a function for type conversion, it is easier to understand the role than using refineDeserializationType, which can handle a variety of content.

--- src/main/kotlin/io/github/projectmapk/jackson/module/kogera/annotation_introspector/KotlinFallbackAnnotationIntrospector.kt
+++ src/main/kotlin/io/github/projectmapk/jackson/module/kogera/annotation_introspector/KotlinFallbackAnnotationIntrospector.kt
@@ -2,14 +2,12 @@ package io.github.projectmapk.jackson.module.kogera.annotation_introspector
 
 import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.databind.JsonSerializer
-import com.fasterxml.jackson.databind.cfg.MapperConfig
 import com.fasterxml.jackson.databind.introspect.Annotated
 import com.fasterxml.jackson.databind.introspect.AnnotatedClass
 import com.fasterxml.jackson.databind.introspect.AnnotatedMember
 import com.fasterxml.jackson.databind.introspect.AnnotatedMethod
 import com.fasterxml.jackson.databind.introspect.AnnotatedParameter
 import com.fasterxml.jackson.databind.introspect.NopAnnotationIntrospector
-import com.fasterxml.jackson.databind.type.TypeFactory
 import com.fasterxml.jackson.databind.util.Converter
 import io.github.projectmapk.jackson.module.kogera.KotlinDuration
 import io.github.projectmapk.jackson.module.kogera.ReflectionCache
@@ -110,54 +108,6 @@ internal class KotlinFallbackAnnotationIntrospector(
         cache.findValueClassReturnType(am)?.let {
             if (it.requireRebox()) cache.getValueClassBoxConverter(am.rawReturnType, it).delegatingSerializer else null
         }
-    }
-
-    /*
-     * ClosedRange, which is not a concrete type like IntRange, does not have a type to deserialize to,
-     * so deserialization by ClosedRangeMixin does not work.
-     * Therefore, this process provides a concrete type.
-     *
-     * The target of processing is ClosedRange and interfaces or abstract classes that inherit from it.
-     * As of Kotlin 1.5.32, ClosedRange and ClosedFloatingPointRange are processed.
-     */
-    override fun refineDeserializationType(config: MapperConfig<*>, a: Annotated, baseType: JavaType): JavaType {
-        return (a as? AnnotatedClass)
-            ?.let { _ ->
-                a.rawType.apply {
-                    if (this != ClosedRange::class.java && this != ClosedFloatingPointRange::class.java) return@let null
-                }
-
-                baseType.bindings.typeParameters.firstOrNull()
-                    ?.let { ClosedRangeHelpers.findClosedFloatingPointRangeRef(it.rawClass) }
-                    ?: ClosedRangeHelpers.comparableRangeClass?.let {
-                        val factory = config.typeFactory
-                        factory.constructParametricType(it, a.type.bindings)
-                    }
-            } ?: baseType
-    }
-}
-
-// At present, it depends on the private class, but if it is made public, it must be switched to a direct reference.
-// see https://youtrack.jetbrains.com/issue/KT-55376
-internal object ClosedRangeHelpers {
-    val closedDoubleRangeRef: JavaType? by lazy {
-        runCatching { Class.forName("kotlin.ranges.ClosedDoubleRange") }.getOrNull()
-            ?.let { TypeFactory.defaultInstance().constructType(it) }
-    }
-
-    val closedFloatRangeRef: JavaType? by lazy {
-        runCatching { Class.forName("kotlin.ranges.ClosedFloatRange") }.getOrNull()
-            ?.let { TypeFactory.defaultInstance().constructType(it) }
-    }
-
-    fun findClosedFloatingPointRangeRef(contentType: Class<*>): JavaType? = when (contentType) {
-        Double::class.javaPrimitiveType, Double::class.javaObjectType -> closedDoubleRangeRef
-        Float::class.javaPrimitiveType, Float::class.javaObjectType -> closedFloatRangeRef
-        else -> null
-    }
-
-    val comparableRangeClass: Class<*>? by lazy {
-        runCatching { Class.forName("kotlin.ranges.ComparableRange") }.getOrNull()
     }
 }
 

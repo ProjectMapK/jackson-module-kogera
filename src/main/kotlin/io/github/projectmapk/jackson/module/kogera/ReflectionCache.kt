39bf2dec87f97befed376c79aadefb0af59fcd7f
wrongwrong <boranti1995@gmail.com>
2023-11-04 21:35:53 +0900

    Reduced cache overhead

--- src/main/kotlin/io/github/projectmapk/jackson/module/kogera/ReflectionCache.kt
+++ src/main/kotlin/io/github/projectmapk/jackson/module/kogera/ReflectionCache.kt
@@ -13,40 +13,41 @@ internal class ReflectionCache(initialCacheSize: Int, maxCacheSize: Int) : Seria
         private const val serialVersionUID = 4L
     }
 
-    private sealed class CacheKey<K : Any, V : Any> {
+    /**
+     * For frequently used JmClass and BoxedReturnType, reduce overhead by using Class and Method directly as key.
+     * For other caches, if the key type overlaps, wrap it.
+     */
+    private sealed class OtherCacheKey<K : Any, V : Any> {
         abstract val key: K
 
-        final override fun equals(other: Any?): Boolean =
-            (other as? CacheKey<*, *>)?.let { it::class == this::class && it.key == key } ?: false
+        // The comparison was implemented directly because the decompiled results showed subtle efficiency.
 
-        final override fun hashCode(): Int = key.hashCode()
+        final override fun equals(other: Any?): Boolean =
+            (other as? OtherCacheKey<*, *>)?.let { it::class == this::class && it.key == key } ?: false
+
+        // If the hashCode matches the raw key, the search efficiency is reduced, so it is displaced.
+        final override fun hashCode(): Int = key.hashCode() * 31
         final override fun toString(): String = key.toString()
 
-        class JmClass(
-            override val key: Class<*>
-        ) : CacheKey<Class<*>, io.github.projectmapk.jackson.module.kogera.JmClass>()
-        class ValueClassReturnType(override val key: Method) : CacheKey<Method, Optional<Class<*>>>()
         class ValueClassBoxConverter(
             override val key: Class<*>
-        ) : CacheKey<Class<*>, io.github.projectmapk.jackson.module.kogera.ValueClassBoxConverter<*, *>>()
+        ) : OtherCacheKey<Class<*>, io.github.projectmapk.jackson.module.kogera.ValueClassBoxConverter<*, *>>()
         class ValueClassUnboxConverter(
             override val key: Class<*>
-        ) : CacheKey<Class<*>, io.github.projectmapk.jackson.module.kogera.ValueClassUnboxConverter<*>>()
+        ) : OtherCacheKey<Class<*>, io.github.projectmapk.jackson.module.kogera.ValueClassUnboxConverter<*>>()
     }
 
-    private val cache = LRUMap<CacheKey<*, *>, Any>(initialCacheSize, maxCacheSize)
-    private fun <T : Any> find(key: CacheKey<*, T>): T? = cache[key]?.let {
+    private val cache = LRUMap<Any, Any>(initialCacheSize, maxCacheSize)
+    private fun <T : Any> find(key: Any): T? = cache[key]?.let {
         @Suppress("UNCHECKED_CAST")
         it as T
     }
 
     @Suppress("UNCHECKED_CAST")
-    private fun <T : Any> putIfAbsent(key: CacheKey<*, T>, value: T): T = cache.putIfAbsent(key, value) as T? ?: value
+    private fun <T : Any> putIfAbsent(key: Any, value: T): T = cache.putIfAbsent(key, value) as T? ?: value
 
     fun getJmClass(clazz: Class<*>): JmClass? {
-        val key = CacheKey.JmClass(clazz)
-
-        return find(key) ?: run {
+        return find(clazz) ?: run {
             val metadata = clazz.getAnnotation(Metadata::class.java) ?: return null
 
             // Do not parse super class for interfaces.
@@ -61,7 +62,7 @@ internal class ReflectionCache(initialCacheSize: Int, maxCacheSize: Int) : Seria
             val interfaceJmClasses = clazz.interfaces.mapNotNull { getJmClass(it) }
 
             val value = JmClass(clazz, metadata, superJmClass, interfaceJmClasses)
-            putIfAbsent(key, value)
+            putIfAbsent(clazz, value)
         }
     }
 
@@ -75,8 +76,7 @@ internal class ReflectionCache(initialCacheSize: Int, maxCacheSize: Int) : Seria
 
     // Return boxed type on Kotlin for unboxed getters
     fun findBoxedReturnType(getter: Method): Class<*>? {
-        val key = CacheKey.ValueClassReturnType(getter)
-        val optional = find(key)
+        val optional = find<Optional<Class<*>>>(getter)
 
         return if (optional != null) {
             optional
@@ -87,17 +87,17 @@ internal class ReflectionCache(initialCacheSize: Int, maxCacheSize: Int) : Seria
             if (getter.returnType.isUnboxableValueClass()) return null
 
             val value = Optional.ofNullable(getter.getValueClassReturnType())
-            putIfAbsent(key, value)
+            putIfAbsent(getter, value)
         }.orElse(null)
     }
 
     fun getValueClassBoxConverter(unboxedClass: Class<*>, valueClass: Class<*>): ValueClassBoxConverter<*, *> {
-        val key = CacheKey.ValueClassBoxConverter(valueClass)
+        val key = OtherCacheKey.ValueClassBoxConverter(valueClass)
         return find(key) ?: putIfAbsent(key, ValueClassBoxConverter(unboxedClass, valueClass))
     }
 
     fun getValueClassUnboxConverter(valueClass: Class<*>): ValueClassUnboxConverter<*> {
-        val key = CacheKey.ValueClassUnboxConverter(valueClass)
+        val key = OtherCacheKey.ValueClassUnboxConverter(valueClass)
         return find(key) ?: putIfAbsent(key, ValueClassUnboxConverter(valueClass))
     }
 }

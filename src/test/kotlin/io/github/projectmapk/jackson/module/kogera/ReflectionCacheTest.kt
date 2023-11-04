ada5925d5128a663d539a4c88aa9ff19c7cb6e19
wrongwrong <boranti1995@gmail.com>
2023-11-04 18:58:07 +0900

    Add tests

--- src/test/kotlin/io/github/projectmapk/jackson/module/kogera/ReflectionCacheTest.kt
+++ src/test/kotlin/io/github/projectmapk/jackson/module/kogera/ReflectionCacheTest.kt
@@ -1,11 +1,12 @@
 package io.github.projectmapk.jackson.module.kogera
 
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 
-class ReflectionCacheTest {
+private class ReflectionCacheTest {
     @Nested
     inner class JDKSerializabilityTest {
         @Test
@@ -34,6 +35,68 @@ class ReflectionCacheTest {
                 // Deserialized instance also do not raise exceptions
                 deserialized.getJmClass(this::class.java)
             }
+        }
+    }
+
+    @JvmInline
+    value class Value(val v: Int)
+    data class ValueWrapper(val value: Value)
+
+    @Nested
+    inner class CachedValueTest {
+        val reflectionCache = ReflectionCache(2)
+
+        @Test
+        fun getJmClassTest() {
+            val v1 = reflectionCache.getJmClass(ReflectionCacheTest::class.java)
+            val v2 = reflectionCache.getJmClass(ReflectionCacheTest::class.java)
+            // The same instance is returned
+            assertTrue(v1 === v2)
+
+            reflectionCache.getJmClass(Value::class.java)
+            val v3 = reflectionCache.getJmClass(ReflectionCacheTest::class.java)
+            assertTrue(v1 === v3)
+
+            reflectionCache.getJmClass(ValueWrapper::class.java)
+            reflectionCache.getJmClass(Value::class.java) // for clear entry
+            val v4 = reflectionCache.getJmClass(ReflectionCacheTest::class.java)
+            assertTrue(v1 !== v4)
+        }
+
+        // findBoxedReturnType wasn't tested because it returns Class and therefore cannot instance comparison
+
+        @Test
+        fun getValueClassBoxConverterTest() {
+            val v1 = reflectionCache.getValueClassBoxConverter(Int::class.java, Value::class.java)
+            val v2 = reflectionCache.getValueClassBoxConverter(Int::class.java, Value::class.java)
+            // The same instance is returned
+            assertTrue(v1 === v2)
+
+            reflectionCache.getJmClass(Value::class.java)
+            val v3 = reflectionCache.getValueClassBoxConverter(Int::class.java, Value::class.java)
+            assertTrue(v1 === v3)
+
+            reflectionCache.getJmClass(ValueWrapper::class.java)
+            reflectionCache.getJmClass(Value::class.java) // for clear entry
+            val v4 = reflectionCache.getValueClassBoxConverter(Int::class.java, Value::class.java)
+            assertTrue(v1 !== v4)
+        }
+
+        @Test
+        fun getValueClassUnboxConverterTest() {
+            val v1 = reflectionCache.getValueClassUnboxConverter(Value::class.java)
+            val v2 = reflectionCache.getValueClassUnboxConverter(Value::class.java)
+            // The same instance is returned
+            assertTrue(v1 === v2)
+
+            reflectionCache.getJmClass(Value::class.java)
+            val v3 = reflectionCache.getValueClassUnboxConverter(Value::class.java)
+            assertTrue(v1 === v3)
+
+            reflectionCache.getJmClass(ValueWrapper::class.java)
+            reflectionCache.getJmClass(Value::class.java) // for clear entry
+            val v4 = reflectionCache.getValueClassUnboxConverter(Value::class.java)
+            assertTrue(v1 !== v4)
         }
     }
 }

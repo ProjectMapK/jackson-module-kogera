8702de58e9ba986d4279c1c1ed1293705fbd6a1a
wrongwrong <boranti1995@gmail.com>
2024-01-02 19:47:02 +0900

    Add testCommon

--- src/test/kotlin/io/github/projectmapk/jackson/module/kogera/TestCommons.kt
+++ src/test/kotlin/io/github/projectmapk/jackson/module/kogera/TestCommons.kt
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.core.util.DefaultIndenter
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.ObjectWriter
+import org.junit.jupiter.api.Assertions.assertEquals
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
 
 // This `printer` is used to match the output from Jackson to the newline char of the source code.
 // If this is removed, comparisons will fail in a Windows-like platform.
@@ -15,3 +18,15 @@ internal fun ObjectMapper.testPrettyWriter(): ObjectWriter = this.writer(LF_PRIN
 internal fun Class<*>.isKotlinClass() = declaredAnnotations.any { it is Metadata }
 
 internal val defaultMapper = jacksonObjectMapper()
+
+internal inline fun <reified T : Any> callPrimaryConstructorByParamName(): T = T::class.primaryConstructor!!.run {
+    val args = parameters.associateWith { it.name }
+    return callBy(args)
+}
+
+// Function for comparing non-data classes.
+internal inline fun <reified T : Any> assertReflectEquals(expected: T, actual: T) {
+    T::class.memberProperties.forEach {
+        assertEquals(it.get(expected), it.get(actual))
+    }
+}

5c79a8e0a54d376d0765d07fa5636e00aaa659f7
wrongwrong <boranti1995@gmail.com>
2024-01-19 14:40:33 +0900

    Fix test target names

--- src/test/kotlin/io/github/projectmapk/jackson/module/kogera/zIntegration/deser/valueClass/jsonCreator/HandledByJacksonTest.kt
+++ src/test/kotlin/io/github/projectmapk/jackson/module/kogera/zIntegration/deser/valueClass/jsonCreator/HandledByJacksonTest.kt
@@ -9,38 +9,38 @@ import org.junit.jupiter.api.Test
 // Test for Creator that can be handled by the Jackson mechanism.
 class HandledByJacksonTest {
     @JvmInline
-    value class PrimitiveNullableCreator(val value: Int) {
+    value class PrimitiveMultiParamCreator(val value: Int) {
         companion object {
             // Avoiding unboxing by making the return value of Creator nullable
             @JvmStatic
             @JsonCreator
-            fun creator(first: Int, second: Int): PrimitiveNullableCreator? =
-                PrimitiveNullableCreator(first + second)
+            fun creator(first: Int, second: Int): PrimitiveMultiParamCreator? =
+                PrimitiveMultiParamCreator(first + second)
         }
     }
 
     @Test
     fun primitiveNullableCreatorTest() {
         val mapper = jacksonObjectMapper()
-        val r: PrimitiveNullableCreator = mapper.readValue("""{"first":1,"second":2}""")
-        assertEquals(PrimitiveNullableCreator(3), r)
+        val r: PrimitiveMultiParamCreator = mapper.readValue("""{"first":1,"second":2}""")
+        assertEquals(PrimitiveMultiParamCreator(3), r)
     }
 
     @JvmInline
-    value class NullableObjectNullableCreator(val value: Int?) {
+    value class NullableObjectMiltiParamCreator(val value: Int?) {
         companion object {
             // Avoiding unboxing by making the return value of Creator nullable
             @JvmStatic
             @JsonCreator
-            fun creator(first: Int, second: Int): NullableObjectNullableCreator? =
-                NullableObjectNullableCreator(first + second)
+            fun creator(first: Int, second: Int): NullableObjectMiltiParamCreator? =
+                NullableObjectMiltiParamCreator(first + second)
         }
     }
 
     @Test
     fun nullableObjectNullableCreatorTest() {
         val mapper = jacksonObjectMapper()
-        val r: NullableObjectNullableCreator = mapper.readValue("""{"first":1,"second":2}""")
-        assertEquals(NullableObjectNullableCreator(3), r)
+        val r: NullableObjectMiltiParamCreator = mapper.readValue("""{"first":1,"second":2}""")
+        assertEquals(NullableObjectMiltiParamCreator(3), r)
     }
 }

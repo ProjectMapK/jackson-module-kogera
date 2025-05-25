01369cca996764610de09a3a4c4299a9acbf19e3
wrongwrong <k163377@users.noreply.github.com>
2025-05-25 11:38:30 +0900

    Update kogera version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ val jacksonVersion = libs.versions.jackson.get()
 val generatedSrcPath = "${layout.buildDirectory.get()}/generated/kotlin"
 
 group = groupStr
-version = "${jacksonVersion}-beta24"
+version = "${jacksonVersion}-beta25"
 
 repositories {
     mavenCentral()

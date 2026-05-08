51727b4271f8288faa2d02d6088670a508d22951
wrongwrong <boranti1995@gmail.com>
2026-05-08 12:22:18 +0900

    Update Github474 test for Kotlin 2.4+

--- src/test/kotlin/io/github/projectmapk/jackson/module/kogera/zPorted/test/github/failing/Github474.kt
+++ src/test/kotlin/io/github/projectmapk/jackson/module/kogera/zPorted/test/github/failing/Github474.kt
@@ -7,16 +7,24 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class TestGithub474 {
+    open class Parent(@JsonProperty("parent-prop") val parent: String)
+    class Child(@JsonProperty("child-prop") val child: String) : Parent(child)
+
     @Test
     fun jsonPropertyAnnotationRespectedOnParentClass() {
-        open class Parent(@JsonProperty("parent-prop") val parent: String)
-        class Child(@JsonProperty("child-prop") val child: String) : Parent(child)
-
-        expectFailure<AssertionError>("GitHub #474 has been fixed!") {
+        val assertion = {
             assertEquals(
                 """{"child-prop":"foo","parent-prop":"foo"}""",
                 defaultMapper.writeValueAsString(Child("foo"))
             )
+        }
+
+        if (KotlinVersion(2, 4) <= KotlinVersion.CURRENT) {
+            assertion()
+        } else {
+            expectFailure<AssertionError>("GitHub #474 has been fixed!") {
+                assertion()
+            }
         }
     }
 }
